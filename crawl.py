6d7753c42d1028936bbb1cab2ebbd92760e4d978
Kiiru Gichohi <samuel.kiiru@gmail.com>
2014-04-28 23:08:20 -0400

    added urrlib3

--- crawl.py
+++ crawl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
-import urllib2
+import urllib3
 import urlparse
 import string
 import json
@@ -54,7 +55,7 @@ class PageExtractor():
         absUrl = urlparse.urlunparse(
             (parsedUrl.scheme, hostUrl, parsedUrl.path,
                 parsedUrl.params, parsedUrl.query, parsedUrl.fragment))
-        return absUrl.strip()
+        return (absUrl.partition("#")[0]).strip()
 
     def extractJS(self):
         """Computes a list of JS files needed by the
@@ -90,11 +91,14 @@ class PageExtractor():
                 self.links_to_consider.append(absUrl)
 
 
-def makeConn(parent_link):
+def makeConn(parent_link, conn):
+    """Makes a connection to a web page"""
     hdr = {'User-Agent': "Magic Browser"}
     try:
-        req = urllib2.Request(parent_link, headers=hdr)
-        src = urllib2.urlopen(req).read()
+        #req = urllib3.Request(parent_link, headers=hdr)
+        req = conn.request('GET', parent_link, headers=hdr)
+
+        src = req.data
 
     except (KeyboardInterrupt, SystemExit):
         raise
@@ -104,8 +108,20 @@ def makeConn(parent_link):
     return src
 
 
-def crawl(url):
+def crawl(url, conn):
+    """Takes a url and prints to stdout a list of static assets and links 
+    on each linked page in the domain as a JSON object
 
+    {
+    "Link: {
+        "Links": [], 
+        "Static Assets": [
+            
+        ]
+    }
+}
+
+    """
     consider_links = set()
     seen_links = set()
     sitemap = {}
@@ -113,34 +129,35 @@ def crawl(url):
     while len(consider_links) > 0:
         l = consider_links.pop()
         seen_links.add(l)
-
         try:
-            src = makeConn(l)
-            c = PageExtractor(l, src)
-            assets = c.getAssets()
-            new_links = assets["Consider_Links"]
-            seen_links.update(assets["Static"])
-            sitemap[l] = {"Static Assets": assets[
-                          "Static"], "Links": assets["Links"]}
-
-            for link in new_links:
-
-                if link not in seen_links:
-                    consider_links.add(link)
+            src = makeConn(l, conn)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             continue
+        c = PageExtractor(l, src)
+        assets = c.getAssets()
+        new_links = assets["Consider_Links"]
+        seen_links.update(assets["Static"])
+        sitemap[l] = {"Static Assets": assets[
+                      "Static"], "Links": assets["Links"]}
+        print("Processing ", l, file=sys.stderr)
+        for link in new_links:
+
+            if link not in seen_links:
+                consider_links.add(link)
+
     return sitemap
 if __name__ == "__main__":
     try:
         root = sys.argv[1]
 
     except IndexError:
-        print "  Usage: ./crawl.py link"
-        print "  Example: ./crawl.py http://cool.com/"
+        print("  Usage: ./crawl.py link", file=sys.stderr)
+        print("  Example: ./crawl.py http://cool.com/", file=sys.stderr)
         exit()
     try:
-        print json.dumps(crawl(root), sort_keys=True, indent=4)
+        conn = urllib3.connection_from_url(root)
+        print(json.dumps(crawl(root, conn), sort_keys=True, indent=4))
     except:
-            raise
+        raise
---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import unittest
+
+
+class CrawlerTest(unittest.TestCase):
+
+    def testHi(self):
+        print "hey"
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
